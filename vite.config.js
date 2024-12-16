b89ad577188d43e427bb8dfc3d232c6bf4df7b84
Joaquin Elizondo <joaquin.elizondo@cencosud.com.uy>
2024-12-16 02:32:21 -0300

    Descripción de los cambios realizados

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  root: './', // La raíz será `frontend` porque es donde está el archivo `vite.config.js`
+  build: {
+    outDir: '../dist', // El resultado del build estará fuera de `frontend`, en `../dist`
+    emptyOutDir: true, // Limpia el directorio `dist` antes del build
+  },
   plugins: [react()],
-})
+});
